c9a9a64d9706a1e81471cebb8eb2901cf0fab9a7
Rudeep Khatiwada <khatiwadarudeep@gmail.com>
2021-01-05 13:48:13 +0545

    my project

---
+++ src/Sdata.jsx
@@ -0,0 +1,37 @@
+
+
+const Sdata = [
+    {
+    
+    imgsrc : "https://64.media.tumblr.com/7ae2b67ec972b67e5f2829d1f97cc494/tumblr_psliwePpcK1wkcjiko1_1280.jpg",
+    sname:"DARK",
+    title : "A Netflix Original Series",
+    links : "https://www.netflix.com/np/title/80100172"
+    },
+    {
+
+    imgsrc : "https://collider.com/wp-content/uploads/2018/03/westworld-season-2-poster.jpg",
+    sname: "Westworld",
+    title : "Amazon Prime Series",
+    links : "https://www.amazon.com/Westworld-Season-1/dp/B01MXDAMKE"
+    },{
+    
+    imgsrc : "https://pyxis.nymag.com/v1/imgs/4d1/dcf/a33209cb40d7dbd980ba6cdceddc56b0f6-got-poster-s8-2.rsquare.w1200.jpg",
+    sname:"Game of Thrones",
+    title : "HBO TV series",
+    links : "https://www.hbo.com/game-of-thrones/season-01"
+    },{
+
+    imgsrc : "https://m.media-amazon.com/images/I/71HoryWKeXL._AC_SY741_.jpg",
+    sname : "Peaky Blinders",
+    title : "A Netflix Original Series",
+    links : "https://www.netflix.com/np/title/80002479"
+    },{
+    imgsrc: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/6d0eb73a-589e-4e22-bc86-c45e3e962be9/d1zt0su-9095cbf7-7ed3-48a9-85a4-cb66b59a472e.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvNmQwZWI3M2EtNTg5ZS00ZTIyLWJjODYtYzQ1ZTNlOTYyYmU5XC9kMXp0MHN1LTkwOTVjYmY3LTdlZDMtNDhhOS04NWE0LWNiNjZiNTlhNDcyZS5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.ww3Wo4wxphoF2J97XdT0ILPooOyzpNnuV6OPXl0uo1Y",
+    sname:"Prison Break",
+    title : "A Netflix Original Series",
+    links : "https://www.netflix.com/gb/title/70140425"
+    }
+]
+
+export default Sdata;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Cards from './Cards'
+import './index.css'
+import Sdata from './Sdata';
+
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    
+    <>
+    <h1 className="heading_style">List of top Five TV series in 2021</h1>
+    <Cards 
+    imgsrc= {Sdata[0].imgsrc}
+    sname= {Sdata[0].sname}
+    title= {Sdata[0].title}
+    links= {Sdata[0].links }
+    />
+    <Cards 
+    imgsrc= {Sdata[1].imgsrc}
+    sname= {Sdata[1].sname}
+    title= {Sdata[1].title}
+    links= {Sdata[1].links }
+    />
+    <Cards 
+    imgsrc= {Sdata[2].imgsrc}
+    sname= {Sdata[2].sname}
+    title= {Sdata[2].title}
+    links= {Sdata[2].links }
+    />
+    <Cards 
+    imgsrc= {Sdata[3].imgsrc}
+    sname= {Sdata[3].sname}
+    title= {Sdata[3].title}
+    links= {Sdata[3].links }
+    />
+    <Cards 
+    imgsrc= {Sdata[4].imgsrc}
+    sname= {Sdata[4].sname}
+    title= {Sdata[4].title}
+    links= {Sdata[4].links }
+    />
+    </>,document.getElementById('root')
+    )
